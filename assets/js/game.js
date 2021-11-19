5403bdcda35a02c08a83f7bed4c29e54140d0fe2
Maryia <malakhava@yahoo.com>
2021-11-19 00:55:15 -0500

    multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,11 +7,16 @@ var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
+console.log(enemyNames);
+console.log(enemyNames.length);
+console.log(enemyNames[0]);
+console.log(enemyNames[3]);
+
+// fight function (now with parameter for enemy's name)
 var fight = function(enemyName) {
-  // repeat and execute as long as the enemy-robot is alive 
-  while(playerHealth > 0 && enemyHealth > 0) {
-      //place fight function code block here ...
-      var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+while(playerHealth > 0 && enemyHealth > 0) {
+ // ask if they want fight   
+var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
 // if player picks to "skip" confirm and then stop the loop
 if (promptFight === "skip" || promptFight === "SKIP") {
@@ -27,7 +32,6 @@ if (confirmSkip) {
   break;
  }
 }
-
 
     // remove enemy's health by subtracting the amount set in the playerAttack variable
     enemyHealth = enemyHealth - playerAttack;
@@ -65,12 +69,27 @@ if (confirmSkip) {
    } 
   }; 
   // if no (false), ask question again by running fight() again
- 
-  
-     
- 
 for (var i = 0; i < enemyNames.length; i++) {
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    fight(pickedEnemyName);
-  }
+   // if player is still alive, keep fighting 
+    if (playerHealth > 0) {   
+        // Alert players that they are starting the round
+    window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+        
+        // pick new enemy to fight based on the index of the enemyNames array
+        var pickedEnemyName = enemyNames[i];
+        
+        // reset enemyHealth before starting new fight
+            enemyHealth = 50;
+        
+        // use debugger to pause script from running and check what's going on at that moment in the code
+        // debugger;
+        
+        // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
+            fight(pickedEnemyName);
+        } 
+        
+        else {
+            window.alert("You have lost your robot in battle! Game Over!");
+            break;
+          }
+}
